5321e978f454dee48a991c1018e412d56f23b04b
MattHubbell <m.r.hubbell@gmail.com>
2018-10-20 16:48:35 -0400

    Add files via upload

---
+++ pkginstall.bat
@@ -0,0 +1,16 @@
+npm install -g @angular/cli
+npm install --save @angular/material @angular/cdk
+npm install --save @angular/animations
+npm install rxjs-compat
+npm install --save hammerjs
+npm install angular-froala-wysiwyg
+npm install -g firebase-tools
+npm install angularfire2 firebase --save
+npm install --save @ng-bootstrap/ng-bootstrap
+npm install ng2-validation --save
+npm install --save angular2-csv@0.2.5
+npm install @angular/flex-layout@latest --save
+npm install --save web-animations-js
+npm install libphonenumber-js --save
+npm install /Users/matt/Downloads/wijmo-5.20181.462/NpmImages/wijmo-amd-min
+ng set defaults.styleExt scss
